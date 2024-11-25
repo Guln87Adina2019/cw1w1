0b77ce885928657bbab28fd89570b77b45937813
Gulnara <adinadzulia@gmail.com>
2024-11-25 11:01:42 +0500

    24.11.2014

---
+++ bakilau.js
@@ -0,0 +1,77 @@
+// Aralyk bakilau: 1tapsirma
+//     let a = +prompt("Birinshi sandi engiz") 
+//     let b = +prompt("Ekinshi sandi engiz")  
+//     
+// while (a , b) {
+//     if(a + b) {
+//     alert( a + b) 
+//     }
+//     if(a - b) {
+//         alert( a - b) 
+//         }
+//         if(a * b) {
+//             alert( a * b  ) 
+//             }
+//             if(a / b) {
+//                 alert( a /  b ) 
+//                 }
+// }
+// if(isNaN(a)&&)
+//     (a+b)
+// alert (`Error`)
+
+      
+
+// 2tapsirma
+  
+
+let pro =prompt("Kajet zatiniz")
+let baga =+prompt("bagasi")
+let a = 5000;
+let b = 10000;
+let c = 20000;
+
+while(pro,baga) {
+    if(baga>5000 || baga <10000){
+        alert("Quttiqtaymiz,sizge jenildik -10%")
+  
+    }
+    if (baga>10000 ||baga>20000){
+        alert("Quttiqtaymiz,sizge jenildik -20%")
+    }
+    
+   
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
